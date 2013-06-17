c3c05a275520badd1b60a2a6d79b9506d4cf4e64
David Villa Alises <david.villa@gmail.com>
2013-06-17 20:05:57 +0200

    "pre div"

--- lib/jekyll/jekyll-import/drupal6.rb
+++ lib/jekyll/jekyll-import/drupal6.rb
@@ -65,7 +65,7 @@ module JekyllImport
         QUERY[" node_revisions "] = " " + prefix + "node_revisions "
       end
 
-      FileUtils.mkdir_p "_posts"
+      FileUtils.mkdir_p "_posts/migrated"
       FileUtils.mkdir_p "_drafts"
 
       # Create the refresh layout
@@ -120,7 +120,7 @@ EOF
         created = post[:created]
         time = Time.at(created)
         is_published = post[:status] == 1
-        dir = is_published ? "_posts" : "_drafts"
+        dir = is_published ? "_posts/migrated" : "_drafts"
         slug = title.strip.downcase.gsub(/(&|&amp;)/, ' and ').gsub(/[\s\.\/\\]/, '-').gsub(/[^\w-]/, '').gsub(/[-_]{2,}/, '-').gsub(/^[-_]/, '').gsub(/[-_]$/, '')
 
         category = self.get_category_for_node(db, node_id)
@@ -165,59 +165,35 @@ EOF
         content.gsub!('[code]',                   "\n<temp-div>\n{% highlight text %}")
 
 
-        content.gsub!(/\[code\s*\w+=["']?shell["']?\s*[\w=]*\]/,       "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-        content.gsub!(/\[code\s*\w+=["']?console["']?\s*[\w=]*\]/,     "\n<temp-div class=\"console\">\n{% highlight console %}\n")
+        content.gsub!(/\[code\s*\w+=["']?shell["']?\s*[\w=]*\]/,       "\n<pre div class=\"console\">\n{% highlight console %}\n")
+        content.gsub!(/\[code\s*\w+=["']?console["']?\s*[\w=]*\]/,     "\n<pre div class=\"console\">\n{% highlight console %}\n")
 
-        content.gsub!(/\[code\s*\w+=["']?html4strict["']?\s*[\w=]*\]/, "\n<temp-div>\n{% highlight html %}")
-        content.gsub!(/\[code\s*\w+=["']?lisp["']?\s*[\w=]*\]/,                  "\n<temp-div>\n{% highlight text %}")
-        content.gsub!(/\[code\s*\w+=["']?none["']?\s*[\w=]*\]/,                  "\n<temp-div>\n{% highlight text %}")
-        content.gsub!(/\[code\s*\w+=["']?XML["']?\s*[\w=]*\]/,         "\n<temp-div>\n{% highlight xml %}")
-        content.gsub!(/\[code\s*\w+=["']?INI["']?\s*[\w=]*\]/,         "\n<temp-div>\n{% highlight ini %}")
-        content.gsub!(/\[code\s*\w+=Makefile\s*[\w=]*\]/,              "\n<temp-div>\n{% highlight make %}")
-        content.gsub!(/\[code\s*\w+=PHP\s*[\w=]*\]/,                   "\n<temp-div>\n{% highlight php %}")
-        content.gsub!(/\[code\s*\w+=["']?(\w+)["']?(\s+[\w=\d]*)*\]/,  "\n<temp-div>\n{% highlight \\1 %}")
-
-#        content.gsub!("[code class=shell]",       "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-#        content.gsub!("[code type=shell]",        "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-#        content.gsub!("[code class=console]",     "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-#        content.gsub!("[code type=console]",      "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-#        content.gsub!("[code lang=console]",      "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-#        content.gsub!("[code language=console]",  "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-
-#         content.gsub!("[code class=bash]",        "\n<temp-div>\n{% highlight bash %}")
-#         content.gsub!("[code type=bash]",         "\n<temp-div>\n{% highlight bash %}")
-#         content.gsub!("[code type=python]",       "\n<temp-div>\n{% highlight python %}")
-#         content.gsub!("[code lang=python]",       "\n<temp-div>\n{% highlight python %}")
-#         content.gsub!("[code language=python]",   "\n<temp-div>\n{% highlight python %}")
-#         content.gsub!("[code lang=php]",          "\n<temp-div>\n{% highlight php %}")
-#         content.gsub!("[code type=PHP]",          "\n<temp-div>\n{% highlight php %}")
-#         content.gsub!("[code class=c]",           "\n<temp-div>\n{% highlight c %}")
-#         content.gsub!("[code type=c]",            "\n<temp-div>\n{% highlight c %}")
-#         content.gsub!("[code class=cpp]",         "\n<temp-div>\n{% highlight cpp %}")
-#         content.gsub!("[code type=cpp]",          "\n<temp-div>\n{% highlight cpp %}")
-#         content.gsub!("[code class=html4strict]", "\n<temp-div>\n{% highlight html %}")
-#         content.gsub!("[code type=mysql]",        "\n<temp-div>\n{% highlight sql %}")
-#         content.gsub!("[code language=ini]",      "\n<temp-div>\n{% highlight ini %}")
-#         content.gsub!("[code type=Makefile]",     "\n<temp-div>\n{% highlight make %}")
-#         content.gsub!("[code type=make]",         "\n<temp-div>\n{% highlight make %}")
-#         content.gsub!("[code lang=Makefile]",     "\n<temp-div>\n{% highlight make %}")
-#         content.gsub!("[code lang=make]",         "\n<temp-div>\n{% highlight make %}")
-        content.gsub!('[/code]',                  "{% endhighlight %}\n</temp-div>")
+        content.gsub!(/\[code\s*\w+=["']?html4strict["']?\s*[\w=]*\]/, "\n<pre div>\n{% highlight html %}")
+        content.gsub!(/\[code\s*\w+=["']?lisp["']?\s*[\w=]*\]/,        "\n<pre div>\n{% highlight text %}")
+        content.gsub!(/\[code\s*\w+=["']?none["']?\s*[\w=]*\]/,        "\n<pre div>\n{% highlight text %}")
+        content.gsub!(/\[code\s*\w+=["']?XML["']?\s*[\w=]*\]/,         "\n<pre div>\n{% highlight xml %}")
+        content.gsub!(/\[code\s*\w+=["']?INI["']?\s*[\w=]*\]/,         "\n<pre div>\n{% highlight ini %}")
+        content.gsub!(/\[code\s*\w+=Makefile\s*[\w=]*\]/,              "\n<pre div>\n{% highlight make %}")
+        content.gsub!(/\[code\s*\w+=PHP\s*[\w=]*\]/,                   "\n<pre div>\n{% highlight php %}")
+        content.gsub!(/\[code\s*\w+=["']?(\w+)["']?(\s+[\w=\d]*)*\]/,  "\n<pre div>\n{% highlight \\1 %}")
 
 
-        content.gsub!('<kbd>',                    "\n<temp-div class=\"console\">\n{% highlight console %}\n")
-        content.gsub!('</kbd>',                   "{% endhighlight %}\n</temp-div>")
-        content.gsub!('</kdb>',                   "{% endhighlight %}\n</temp-div>")
+        content.gsub!('[/code]',                  "{% endhighlight %}\n</pre div>")
+
+
+        content.gsub!('<kbd>',                    "\n<pre div class=\"console\">\n{% highlight console %}\n")
+        content.gsub!('</kbd>',                   "\n{% endhighlight %}\n</pre div>")
+        content.gsub!('</kdb>',                   "\n{% endhighlight %}\n</pre div>")
 
         content.gsub!('<notextile>', '')
         content.gsub!('</notextile>', '')
 
 
         content.gsub!(':http', '::ttp')
-#        content = Rinku.auto_link(content, mode=:urls, skip_tags="temp-div")
+        content = Rinku.auto_link(content, mode=:urls)
         content.gsub!('::ttp', ':http')
 
-        content.gsub!('temp-div', 'div')
+        content.gsub!('pre div', 'div')
 
         parts = content.split('<!--break-->')
         if parts[1] and parts[1].length < 10
@@ -238,8 +214,8 @@ EOF
           aliases.push(:dst => "node/#{node_id}")
 
           aliases.each do |url_alias|
-            FileUtils.mkdir_p url_alias[:dst]
-            File.open("#{url_alias[:dst]}/index.md", "w") do |f|
+            FileUtils.mkdir_p "p/%s" % url_alias[:dst]
+            File.open("p/#{url_alias[:dst]}/index.md", "w") do |f|
               f.puts "---"
               f.puts "layout: refresh"
               f.puts "refresh_to_post_id: /#{category}/#{time.strftime("%Y-%m-%d/") + slug}"
